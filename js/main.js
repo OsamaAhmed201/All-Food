b2909aea5a1847783331421b24c8b033579aec24
OsamaAhmed201 <oa3885430@gmail.com>
2023-03-21 22:30:15 +0200

    initial commit

---
+++ js/main.js
@@ -0,0 +1,38 @@
+$('.your-class').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 1000,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    }
+  
+  ]
+
+});
+      
+
+
+
+ 
